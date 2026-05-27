410e0937c9cd254f80eb5646fee870cdcc06224a
Adam Örn <vt25ador@axevalla.fhsk.se>
2026-05-27 10:19:46 +0200

    update

--- Assets/Scripts/EnemyFolder/EnemyHealth.cs
+++ Assets/Scripts/EnemyFolder/EnemyHealth.cs
@@ -1,157 +1,208 @@
-using NUnit.Framework;
-using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.VFX;
 
 public class EnemyHealth : MonoBehaviour
 {
+    [Header("Health")]
     [SerializeField] private float _maxHealth = 50f;
-    [SerializeField] private bool _destroyOnDeath = true;
 
+    [Header("Death")]
+    [SerializeField] private bool _destroyOnDeath = false;
+    [SerializeField] private float destroyDelay = 5f;
+
+    [Header("Corpse Visual")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    [Tooltip("Om du bara vill ha ett lik.")]
+    [SerializeField] private Sprite deadSprite;
+
+    [Tooltip("Om du vill ha flera olika lik. Om denna har sprites väljs en random.")]
+    [SerializeField] private Sprite[] randomDeadSprites;
+
+    [SerializeField] private bool useRandomCorpseRotation = true;
+    [SerializeField] private float randomRotationMin = -25f;
+    [SerializeField] private float randomRotationMax = 25f;
+
+    [Header("Corpse Sorting")]
+    [SerializeField] private bool changeSortingOnDeath = true;
+    [SerializeField] private int corpseOrderInLayer = -1;
+
+    [Header("VFX")]
     [SerializeField] private GameObject HitVFX;
 
     private float _currentHealth;
     private bool _isDead;
 
-    private MonoBehaviour[] _behaviousToDisable;
+    private MonoBehaviour[] _behavioursToDisable;
     private Rigidbody2D _rb2d;
     private Collider2D _col2d;
 
+    public bool IsDead => _isDead;
+
+    public float ReimainingHealthPercentage
+    {
+        get
+        {
+            if (_maxHealth <= 0f)
+                return 0f;
+
+            return (_currentHealth / _maxHealth) * 100f;
+        }
+    }
+
     private void Awake()
     {
-        var all = GetComponents<MonoBehaviour>();
-        var list = new List<MonoBehaviour>(all.Length);
+        MonoBehaviour[] all = GetComponents<MonoBehaviour>();
+        List<MonoBehaviour> list = new List<MonoBehaviour>(all.Length);
 
-        foreach (var mb in all)
+        foreach (MonoBehaviour mb in all)
         {
-            if (mb == this) continue;
+            if (mb == this)
+                continue;
+
             list.Add(mb);
         }
-        _behaviousToDisable = list.ToArray();
 
-        TryGetComponent<Rigidbody2D>(out _rb2d);
-        TryGetComponent<Collider2D>(out _col2d);
+        _behavioursToDisable = list.ToArray();
+
+        TryGetComponent(out _rb2d);
+        TryGetComponent(out _col2d);
+
+        if (spriteRenderer == null)
+            spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Start()
     {
-       _currentHealth = _maxHealth;
+        _currentHealth = _maxHealth;
         _isDead = false;
     }
 
-
     public void TakeDamage(float amount)
     {
-        if (_isDead || amount <= 0f) return;
-        
-        _currentHealth -= amount;
-        if(_currentHealth < 0f) _currentHealth = 0f;
+        if (_isDead || amount <= 0f)
+            return;
 
-        //_currentHealth = Mathf.Max(0f, _currentHealth);
+        _currentHealth -= amount;
+        _currentHealth = Mathf.Clamp(_currentHealth, 0f, _maxHealth);
 
         SpawnHitVFX();
 
         if (_currentHealth <= 0f)
             Die();
-
     }
 
     public void AddHealth(float amount)
     {
-        if (_isDead || amount <= 0f) return;
-        _currentHealth = Mathf.Min(_maxHealth, _currentHealth + amount);
+        if (_isDead || amount <= 0f)
+            return;
 
-        if (KillScore.Instance != null)
-        {
-            KillScore.Instance.AddKill();
-        }
+        _currentHealth = Mathf.Clamp(_currentHealth + amount, 0f, _maxHealth);
     }
 
-    public float ReimainingHealthPercentage =>  (_maxHealth <= 0f) ? 0f : (_currentHealth / _maxHealth) * 100f;
+    private void Die()
+    {
+        if (_isDead)
+            return;
 
+        _isDead = true;
+
+        // Om special infected håller fast player, släpp den
+        //JockeyEnemy2D jockey = GetComponent<JockeyEnemy2D>();
+        //if (jockey != null)
+            //jockey.ReleaseVictim();
+
+        ApplyCorpseVisual();
+
+        DisableEnemyBehaviour();
+
+        if (KillScore.Instance != null)
+            KillScore.Instance.AddKill();
+
+        if (_destroyOnDeath)
+            Destroy(gameObject, destroyDelay);
+    }
+
+    private void ApplyCorpseVisual()
+    {
+        if (spriteRenderer == null)
+            return;
+
+        Sprite selectedCorpse = GetCorpseSprite();
+
+        if (selectedCorpse != null)
+            spriteRenderer.sprite = selectedCorpse;
+
+        if (useRandomCorpseRotation)
+        {
+            float randomZ = Random.Range(randomRotationMin, randomRotationMax);
+            transform.rotation = Quaternion.Euler(0f, 0f, randomZ);
+        }
+
+        if (changeSortingOnDeath)
+            spriteRenderer.sortingOrder = corpseOrderInLayer;
+    }
+
+    private Sprite GetCorpseSprite()
+    {
+        if (randomDeadSprites != null && randomDeadSprites.Length > 0)
+        {
+            int index = Random.Range(0, randomDeadSprites.Length);
+
+            if (randomDeadSprites[index] != null)
+                return randomDeadSprites[index];
+        }
+
+        return deadSprite;
+    }
+
+    private void DisableEnemyBehaviour()
+    {
+        for (int i = 0; i < _behavioursToDisable.Length; i++)
+        {
+            MonoBehaviour mb = _behavioursToDisable[i];
+
+            if (mb != null)
+                mb.enabled = false;
+        }
+
+        if (_rb2d != null)
+        {
+            _rb2d.linearVelocity = Vector2.zero;
+            _rb2d.angularVelocity = 0f;
+            _rb2d.simulated = false;
+        }
+
+        if (_col2d != null)
+            _col2d.enabled = false;
+    }
 
     private void SpawnHitVFX()
     {
         if (HitVFX == null)
             return;
+
+        GameObject fx = Instantiate(HitVFX, transform.position, Quaternion.identity);
+
+        ParticleSystem ps = fx.GetComponent<ParticleSystem>();
+
+        if (ps != null)
         {
-            {
-                Debug.LogWarning("HitVFX is not assigned on " + gameObject.name);
-            }
+            ParticleSystem.MainModule main = ps.main;
+            float life = main.duration + main.startLifetime.constantMax;
+            Destroy(fx, life + 0.1f);
+            return;
+        }
 
-            var fx = Instantiate(HitVFX, transform.position, Quaternion.identity);
+        VisualEffect vfx = fx.GetComponent<VisualEffect>();
 
-            var ps = fx.GetComponent<ParticleSystem>();
-            if (ps != null)
-            {
-                var main = ps.main;
-                float life = main.duration + main.startLifetime.constantMax;
-                Destroy(fx, life + 0.1f);
-                return;
-            }
-
-            var vfx = fx.GetComponent<VisualEffect>();
-            if (vfx != null)
-            {
-                Destroy(fx, 2f);
-                return;
-            }
-
+        if (vfx != null)
+        {
             Destroy(fx, 2f);
-        }
-    }
-
-    private void Die()
-    {
-        if (_isDead) return;
-        _isDead = true;
-
-        {
-            for (int i = 0; i < _behaviousToDisable.Length; i++)
-            {
-                var mb = _behaviousToDisable[i];
-                if (mb != null)
-                    mb.enabled = false;
-            }
+            return;
         }
 
-        if (_rb2d != null) _rb2d.simulated = false;
-        if (_col2d != null) _col2d.enabled = false;
-
-        if (_destroyOnDeath)
-            Destroy(gameObject, 5f);
-
-        KillScore.Instance.AddKill();
-        Destroy(gameObject);
-    
-
-
-        //var mbs = GetComponents<MonoBehaviour>();
-        //foreach (var mb in mbs)
-        //{
-        //if (mb != this)
-        //mb.enabled = false;
-        //}
-
-        //var rb2d = GetComponent<Rigidbody2D>();
-        //if (rb2d != null) rb2d.simulated = false;
-
-        //var cold2d = GetComponent<Collider2D>();
-        //if (cold2d != null) cold2d.enabled = false;
-
-        //if(_destroyOnDeath)StartCoroutine(DespawnAfterDelay(5f));
-
-
-        //if (_destroyOnDeath)
-        //Destroy(gameObject);
+        Destroy(fx, 2f);
     }
-
-    //private IEnumerator DespawnAfterDelay(float delay)
-    //{
-    //yield return new WaitForSeconds(delay);
-    //Destroy(gameObject);
-    //}
 }
-
---
+++ Assets/Scripts/EnemyFolder/EnemyHealthOld.cs
@@ -0,0 +1,157 @@
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class EnemyHealthOld : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth = 50f;
+    [SerializeField] private bool _destroyOnDeath = true;
+
+    [SerializeField] private GameObject HitVFX;
+
+    private float _currentHealth;
+    private bool _isDead;
+
+    private MonoBehaviour[] _behaviousToDisable;
+    private Rigidbody2D _rb2d;
+    private Collider2D _col2d;
+
+    private void Awake()
+    {
+        var all = GetComponents<MonoBehaviour>();
+        var list = new List<MonoBehaviour>(all.Length);
+
+        foreach (var mb in all)
+        {
+            if (mb == this) continue;
+            list.Add(mb);
+        }
+        _behaviousToDisable = list.ToArray();
+
+        TryGetComponent<Rigidbody2D>(out _rb2d);
+        TryGetComponent<Collider2D>(out _col2d);
+    }
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+        _isDead = false;
+    }
+
+
+    public void TakeDamage(float amount)
+    {
+        if (_isDead || amount <= 0f) return;
+
+        _currentHealth -= amount;
+        if (_currentHealth < 0f) _currentHealth = 0f;
+
+        //_currentHealth = Mathf.Max(0f, _currentHealth);
+
+        SpawnHitVFX();
+
+        if (_currentHealth <= 0f)
+            Die();
+
+    }
+
+    public void AddHealth(float amount)
+    {
+        if (_isDead || amount <= 0f) return;
+        _currentHealth = Mathf.Min(_maxHealth, _currentHealth + amount);
+
+        if (KillScore.Instance != null)
+        {
+            KillScore.Instance.AddKill();
+        }
+    }
+
+    public float ReimainingHealthPercentage => (_maxHealth <= 0f) ? 0f : (_currentHealth / _maxHealth) * 100f;
+
+
+    private void SpawnHitVFX()
+    {
+        if (HitVFX == null)
+            return;
+        {
+            {
+                Debug.LogWarning("HitVFX is not assigned on " + gameObject.name);
+            }
+
+            var fx = Instantiate(HitVFX, transform.position, Quaternion.identity);
+
+            var ps = fx.GetComponent<ParticleSystem>();
+            if (ps != null)
+            {
+                var main = ps.main;
+                float life = main.duration + main.startLifetime.constantMax;
+                Destroy(fx, life + 0.1f);
+                return;
+            }
+
+            var vfx = fx.GetComponent<VisualEffect>();
+            if (vfx != null)
+            {
+                Destroy(fx, 2f);
+                return;
+            }
+
+            Destroy(fx, 2f);
+        }
+    }
+
+    private void Die()
+    {
+        if (_isDead) return;
+        _isDead = true;
+
+        {
+            for (int i = 0; i < _behaviousToDisable.Length; i++)
+            {
+                var mb = _behaviousToDisable[i];
+                if (mb != null)
+                    mb.enabled = false;
+            }
+        }
+
+        if (_rb2d != null) _rb2d.simulated = false;
+        if (_col2d != null) _col2d.enabled = false;
+
+        if (_destroyOnDeath)
+            Destroy(gameObject, 5f);
+
+        KillScore.Instance.AddKill();
+        Destroy(gameObject);
+
+
+
+        //var mbs = GetComponents<MonoBehaviour>();
+        //foreach (var mb in mbs)
+        //{
+        //if (mb != this)
+        //mb.enabled = false;
+        //}
+
+        //var rb2d = GetComponent<Rigidbody2D>();
+        //if (rb2d != null) rb2d.simulated = false;
+
+        //var cold2d = GetComponent<Collider2D>();
+        //if (cold2d != null) cold2d.enabled = false;
+
+        //if(_destroyOnDeath)StartCoroutine(DespawnAfterDelay(5f));
+
+
+        //if (_destroyOnDeath)
+        //Destroy(gameObject);
+    }
+
+    //private IEnumerator DespawnAfterDelay(float delay)
+    //{
+    //yield return new WaitForSeconds(delay);
+    //Destroy(gameObject);
+    //}
+}
+
